f15d82b4c889db43a36b1424f21265eb2d904559
mark.tseng <mark.tseng@realtek.com>
2015-08-05 14:15:51 +0800

    [A] add libfoo.so & libfoo.a sample code

---
+++ share_obj/foo.h
@@ -0,0 +1,7 @@
+#ifndef foo_h__
+#define foo_h__
+ 
+extern void foo(void);
+ 
+#endif  // foo_h__
+
---
+++ share_obj/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "foo.h"
+ 
+int main(void)
+{
+    puts("This is a shared library test...");
+    foo();
+    return 0;
+}
+
