4e54711ae7546f7b68e4ab154d995cedc321bc5f
mark.tseng <mark.tseng@realtek.com>
2015-09-02 12:57:35 +0800

    [M] update timer.c example for testing setup_timer & del_timer

--- linux_device_driver_template/linux_module_example/timer.c
+++ linux_device_driver_template/linux_module_example/timer.c
@@ -1,6 +1,11 @@
-#include <linux/kernel.h>
+#include <linux/sched.h>
+#include <linux/sched/rt.h>
 #include <linux/module.h>
-#include <linux/timer.h>
+#include <linux/delay.h>
+#include <asm/io.h>
+#include <rbus/wdog_reg.h>
+#include <linux/platform_device.h>
+#include <linux/freezer.h>
 
 MODULE_LICENSE("GPL");
 
@@ -9,6 +14,7 @@ static struct timer_list my_timer;
 void my_timer_callback( unsigned long data )
 {
 	int ret;
+	ret = del_timer( &my_timer );
 	printk( "my_timer_callback called (%ld).\n", jiffies );
 	ret = mod_timer( &my_timer, jiffies + msecs_to_jiffies(1000) );
 	if (ret) printk("Error in mod_timer\n");
@@ -21,6 +27,7 @@ int init_module( void )
 	printk("Timer module installing\n");
 
 	// my_timer.function, my_timer.data
+	// Note: initial timer key once only
 	setup_timer( &my_timer, my_timer_callback, 0 );
 
 	printk( "Starting timer to fire in 1000ms (%ld)\n", jiffies );
