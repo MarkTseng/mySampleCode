e2019f0c900552392918906dcc83846900d122d0
Mark Tseng <chunjen.tseng@gmail.com>
2016-04-06 02:42:16 +0800

    [M] add ## and # in macro define

--- c_code_template/define_##.c
+++ c_code_template/define_##.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-#define paster( n ) printf( "token" #n " = %d\n", token##n )
-int token9 = 9;
+#define paster( n, mm ) printf( "token" #n " = %d\n", token##mm )
+int token100 = 100;
 
 int main()
 {
-	paster(9);  // preprocess to printf_s( "token" "9" " = %d", token9 );
+	paster(9, 100);  // preprocess to printf_s( "token" "9" " = %d", token9 );
 	return 0;
 }
