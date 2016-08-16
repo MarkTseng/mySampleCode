d0da3c504f625ef7b1fb1d0f89224e218d4783f4
mark <mark.tseng@mstarsemi.com>
2016-08-16 20:45:38 +0800

    [M] add devmem write support

--- linux_device_driver_template/linux_module_example/user-space-program/devmem.c
+++ linux_device_driver_template/linux_module_example/user-space-program/devmem.c
@@ -13,13 +13,17 @@ int main(int argc, char *argv[])
 	unsigned int phys_addr;
 	unsigned int phys_offset;
 	unsigned int phys_len;
+	unsigned int write_enable = 0;
 	unsigned int page_size = getpagesize();
 	unsigned int page_size_mask = getpagesize() - 1;
-	int fd,i;  
+	int fd,i;
+	char * buffer;
 	unsigned int content;  
+	FILE *writeData;
 
-	if(argc < 3) {
-		printf("usage: devmem phys_addr len\n");
+	if(argc < 4) {
+		printf("usage: devmem phys_addr r length\n");
+		printf("       devmem phys_addr w data_file\n");
 		exit(-1);
 	}
 	fd = open("/dev/mem", O_RDWR|O_SYNC);  
@@ -28,16 +32,54 @@ int main(int argc, char *argv[])
 		return (-1);  
 	}  
 	phys_addr = strtol(argv[1],NULL,16);
-	phys_len = strtol(argv[2],NULL,16);
+	// write 
+	if(strncmp(argv[2],"w",1)==0) {
+		write_enable = 1;
+		if(argv[3] == NULL) {
+			printf("no write data file\n");
+			exit(-1);
+		}
+	}else{ // read
+		phys_len = strtol(argv[3],NULL,16);
+		phys_offset = phys_addr & (page_size_mask);
+		if(phys_offset != 0) {
+			phys_addr = phys_addr & (~page_size_mask); // page size alignment
+			phys_len = phys_len + phys_offset;
+		}
+	}
+	// cal write data length
+	if(write_enable == 1)
+	{
+		writeData = fopen(argv[3],"r");
+		if(writeData == NULL){ 
+			printf("open file fail\n");
+			exit(-1);
+		}
+		fseek (writeData , 0 , SEEK_END);
+		phys_len = ftell (writeData);
+		rewind (writeData);
+		buffer = (char*) malloc (sizeof(char)* phys_len);
+		if (buffer == NULL) {fputs ("Memory error",stderr); exit (2);}
+		fread (buffer,1,phys_len,writeData);
+		fclose(writeData);
+		printf("read file: %s, size: %#x\n", argv[3], phys_len);
+	}
+	
+	// align PAGE_SIZE
 	phys_offset = phys_addr & (page_size_mask);
 	if(phys_offset != 0) {
 		phys_addr = phys_addr & (~page_size_mask); // page size alignment
-		phys_len = phys_len + phys_offset;
+		phys_len = phys_len + phys_offset + 1;
 	}
-	printf("start mmap phys addr: %#x, offset: %#x, len: %#x\n", phys_addr, phys_offset, phys_len);
+
+	printf("start mmap phys addr: %#x, offset: %#x, len: %#x, write: %#x\n", phys_addr, phys_offset, phys_len, write_enable);
 	printf("page size = %#x\n",page_size);
 	printf("page mask = %#x\n",page_size_mask);
 
+	if(phys_len == 0) {
+		printf("length could not be 0\n");
+		exit(3);
+	}
 	map_base = mmap(NULL, phys_len, PROT_READ|PROT_WRITE, MAP_SHARED, fd, phys_addr);  
 
 	if (map_base == MAP_FAILED)  
@@ -50,16 +92,24 @@ int main(int argc, char *argv[])
 		printf("mmap base: %p\n",map_base);
 	}  
 
-	for (i=phys_offset;i < phys_len; ++i)  
-	{  
-		content = map_base[i];  
-		printf("address: 0x%p   content 0x%u\n", (map_base +i), content);  
+	// write data to phys memory addr
+	if(write_enable == 1)
+	{
+		for (i=phys_offset;i < phys_len; ++i)  
+		{ 
+			map_base[i] = (unsigned char)buffer[i];  
+			content = map_base[i];  
+			printf("updated address: 0x%p, content: 0x%x, buffer: %#x\n", (map_base+i), (unsigned int)content, buffer[i]);  
+		}
+		free(buffer); 
+	} else { // read data from phys memory addr
+		for (i=phys_offset;i < phys_len; ++i)  
+		{ 
 
-		//map_base[i] = (unsigned char)i;  
-		//content = map_base[i];  
-		//printf("updated address: 0x%lx   content 0x%x\n", addr, (unsigned int)content);  
-	}  
-
+			content = map_base[i];  
+			printf("address: 0x%p   content %#x\n", (map_base +i), content);  
+		}  
+	}
 	close(fd);  
 
 	munmap(map_base, phys_len);  
