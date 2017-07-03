c233db4282b72305e6b64afc7fddedfdb668995f
mark <mark.tseng@mstarsemi.com>
2017-07-03 10:35:36 +0800

    [M] add execute path to replace fixed path name

--- minigdb/trace_pid.c
+++ minigdb/trace_pid.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <unistd.h>
 #include <wait.h>
@@ -12,88 +13,89 @@
 #include <sys/ptrace.h>
 #include <asm/ptrace.h>
 #include <sys/user.h>
-#if 0
-void * trace_process(void * pid){
-    pid_t child = atoi((char *) pid);
-    long orig_eax, eax;
-    int status;
-    int callmade = FALSE;
-    long opt = PTRACE_O_TRACEFORK;
-    long newpid;
 
-    long trace = ptrace(PTRACE_ATTACH,child,NULL,NULL);
-    ptrace(PTRACE_SETOPTIONS,child,NULL,opt);
-    if(trace == FALSE)
-        printf("Attached to %d\n",child);
-
-    while(TRUE) {
-        child = waitpid(-1, &status, __WALL);
-
-        if (status >> 16 == PTRACE_EVENT_FORK) {
-            ptrace(PTRACE_GETEVENTMSG, child, NULL, (long) &newpid);
-            ptrace(PTRACE_SYSCALL, newpid, NULL, NULL);       
-
-            printf("Attached to offspring %ld\n", newpid);  
-        }
-        else{
-            if(WIFEXITED(status))
-                printf("Child %d exited\n", child);
-        }
-
-        ptrace(PTRACE_SYSCALL,child, NULL, NULL);
-    }  
+// global variable
+pid_t g_child;
+pid_t g_mainPid;
+static void signal_handler(int signo)
+{
+	printf("SIGINT trigger\n");
+	ptrace(PTRACE_KILL, g_child,0,0);
+	kill(g_mainPid, SIGKILL);
 }
-#endif
-int main(int argc __attribute__((unused)), char **argv, char **envp) {
 
-	pid_t child;
-	child = fork();
+int main(int argc __attribute__((unused)), char **argv, char **envp) 
+{
+	pid_t new_child;
+
+    if(argc == 1)
+    {
+        fprintf(stderr, "usage memtrace <bin>\n");
+        exit(-1);
+    }
+
+    const char *path = argv[1];
+    const char *name = strrchr(path, '/');
+    if(name){
+        name += 1;        
+    }else{
+        name = path;
+    }
+
+	signal(SIGINT, signal_handler);
+
+	g_child = fork();
     long newpid = 0;
 
-	if (!child) {
+	if (!g_child) {
 
-		ptrace(PTRACE_TRACEME, child,0,0);
-		execve("/ext/bin/mixer",
+		ptrace(PTRACE_TRACEME, g_child,0,0);
+		execve(path,
 				argv, envp);
 
 		return 0;
 
 	} else {
-        printf("child: %d\n", child);
+		g_mainPid = getpid();
+        printf("g_child: %d\n", g_child);
+        printf("g_mainPid: %d\n", g_mainPid);
+			
 		int status,w;
 	
-		child = waitpid(-1, &status, __WALL);
+		new_child = waitpid(-1, &status, __WALL);
 		if (WIFSTOPPED(status)) {
-			printf("### pid: %ld, stop signal: %d\n", child, WSTOPSIG(status));  
-			ptrace(PTRACE_SETOPTIONS, child, NULL, PTRACE_O_TRACECLONE | PTRACE_O_TRACEVFORK | PTRACE_O_TRACEFORK | PTRACE_O_TRACEEXEC);
+			printf("### pid: %ld, stop signal: %d\n", new_child, WSTOPSIG(status));  
+			ptrace(PTRACE_SETOPTIONS, new_child, NULL, PTRACE_O_TRACECLONE | PTRACE_O_TRACEVFORK | PTRACE_O_TRACEFORK | PTRACE_O_TRACEEXEC);
 		}
-		ptrace(PTRACE_CONT,child, NULL, NULL);
+		ptrace(PTRACE_CONT,new_child, NULL, NULL);
 
 		while(1) {
-			child = waitpid(-1, &status, __WALL);
+			new_child = waitpid(-1, &status, __WALL);
 			if (WIFSTOPPED(status)) {
-				printf("### pid: %ld, stop signal: %d\n", child, WSTOPSIG(status));  
-				ptrace(PTRACE_CONT,child, NULL, NULL);
+				printf("### pid: %ld, stop signal: %d\n", new_child, WSTOPSIG(status));  
+				ptrace(PTRACE_CONT,new_child, NULL, NULL);
 			}
 			if (status>>8 == (SIGTRAP | (PTRACE_EVENT_EXEC<<8))) {
-				printf("### PTRACE_EVENT_EXEC %ld, \n", child);  
+				printf("### PTRACE_EVENT_EXEC %ld, \n", new_child);  
 			}
 			if (status>>8 == (SIGTRAP | (PTRACE_EVENT_CLONE<<8))) {
-				printf("### PTRACE_EVENT_CLONE %ld\n", child);  
+				printf("### PTRACE_EVENT_CLONE %ld\n", new_child);  
 			}
 			if (status>>8 == (SIGTRAP | (PTRACE_EVENT_VFORK<<8))) {
-				printf("### PTRACE_EVENT_VLONE %ld\n", child);  
+				printf("### PTRACE_EVENT_VLONE %ld\n", new_child);  
 			}
 			if (status>>8 == (SIGTRAP | (PTRACE_EVENT_VFORK_DONE<<8))) {
-				printf("### PTRACE_EVENT_VFORK_DONE %ld\n", child);  
+				printf("### PTRACE_EVENT_VFORK_DONE %ld\n", new_child);  
 			}
 			if (status >>8 == PTRACE_EVENT_FORK) {
-				printf("### PTRACE_EVENT_FORK %ld\n", child);  
+				printf("### PTRACE_EVENT_FORK %ld\n", new_child);  
 			}
 			if(WIFEXITED(status)) {
-				printf("### Child %d exited\n", child);
+				printf("### new_child %d exited\n", new_child);
+				if(new_child == g_child)
+					break;
 			}
-		}  
+		}
 	}
 
 	return 0;
