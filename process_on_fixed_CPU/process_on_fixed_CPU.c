e098d2329368f4575da6b7e54aa3a501545446e4
mark <mark.tseng@mstarsemi.com>
2018-01-29 11:04:13 +0800

    [M] fixed process_on_fixed_CPU example

--- process_on_fixed_CPU/process_on_fixed_CPU.c
+++ process_on_fixed_CPU/process_on_fixed_CPU.c
@@ -1,8 +1,10 @@
 #define _GNU_SOURCE
-#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <sched.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #define handle_error_en(en, msg) \
 	do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
@@ -12,31 +14,16 @@ main(int argc, char *argv[])
 {
 	int s, j;
 	cpu_set_t cpuset;
-	pthread_t thread;
-
-	thread = pthread_self();
-
-	/* Set affinity mask to include CPUs 0 to 7 */
 
 	CPU_ZERO(&cpuset);
-	for (j = 0; j < 8; j++)
-		CPU_SET(j, &cpuset);
+	CPU_SET(2, &cpuset); // on CPU 2
 
-	s = pthread_setaffinity_np(thread, sizeof(cpu_set_t), &cpuset);
+	s = sched_setaffinity(getpid(), sizeof(cpu_set_t), &cpuset);
 	if (s != 0)
-		handle_error_en(s, "pthread_setaffinity_np");
+		handle_error_en(s, "sched_setaffinity");
 
-	/* Check the actual affinity mask assigned to the thread */
-
-	s = pthread_getaffinity_np(thread, sizeof(cpu_set_t), &cpuset);
-	if (s != 0)
-		handle_error_en(s, "pthread_getaffinity_np");
-
-	printf("Set returned by pthread_getaffinity_np() contained:\n");
-	for (j = 0; j < CPU_SETSIZE; j++)
-		if (CPU_ISSET(j, &cpuset))
-			printf("    CPU %d\n", j);
-
+	// your code here
+	while(1);
 	exit(EXIT_SUCCESS);
 }
 
